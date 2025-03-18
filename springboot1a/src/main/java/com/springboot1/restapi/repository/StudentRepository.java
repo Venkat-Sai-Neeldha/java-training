2ba6fad13b6bf087ccb6443d8d1fe292052d4a43
Venkat-Sai-Neeldha <144706211+Venkat-Sai-Neeldha@users.noreply.github.com>
2025-03-18 14:31:43 +0530

    changes

--- springboot1a/src/main/java/com/springboot1/restapi/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.springboot1.restapi.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.springboot1.restapi.entity.Student;
-
-public interface StudentRepository extends JpaRepository<Student,Integer> {
-
-}
