45f65f37e5da7cd06a52102720454e551cb524f9
Venkat-Sai-Neeldha <144706211+Venkat-Sai-Neeldha@users.noreply.github.com>
2025-03-20 09:48:23 +0530

    java task

---
+++ springbootproj/src/main/java/com/questk2/springbootproj/controller/StudentController.java
@@ -0,0 +1,54 @@
+package com.questk2.springbootproj.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.questk2.springbootproj.entity.Student;
+import com.questk2.springbootproj.repository.StudentRepository;
+
+@RestController
+public class StudentController {
+	@Autowired
+	StudentRepository repo;
+@GetMapping("/students")
+public List<Student>getAllStudents(){
+	List<Student>students=repo.findAll();
+	return students;
+}
+	@GetMapping("/students/{id}")
+	public Student getStudent(@PathVariable int id) {
+		Student student=repo.findById(id).get();
+		return student;
+		
+	}
+	@PostMapping("/student/add")
+	@ResponseStatus(code=HttpStatus.CREATED)
+	public void createStudent(@RequestBody Student student) {
+		repo.save(student);
+		
+	}
+	@PutMapping("/student/update/{id}")
+	public Student  updateStudents(@PathVariable int id,  @RequestBody Student studentRequest) {
+	Student student= repo.findById(id).get();
+	student.setName(studentRequest.getName());
+	student.setPercentage(studentRequest.getPercentage());
+	repo.save(student);
+		return student;
+	}
+	@DeleteMapping("/student/delete/{id}")
+	public void removeStudent(@PathVariable int id) {
+		Student student=repo.findById(id).get();
+		repo.delete(student);
+	}
+}
+
